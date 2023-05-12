25017d4969f9164de5b8e9c1edb1c6fd930ad689
Stanislav_Babkin <stanislav_babkin@epam.com>
2023-05-12 23:02:38 +0300

    delete terraform backend

--- providers.tf
+++ providers.tf
@@ -2,15 +2,6 @@ terraform {
 
   required_version = "= 1.4.6"
 
-  backend "s3" {
-    bucket         = "terraform-states-<AWS_ACCOUNT_ID>"
-    key            = "<PROJECT_NAME>/<REGION>/terraform/terraform.tfstate"
-    region         = "<REGION>"
-    acl            = "bucket-owner-full-control"
-    dynamodb_table = "terraform_locks"
-    encrypt        = true
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
